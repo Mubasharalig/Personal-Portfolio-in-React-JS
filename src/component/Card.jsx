24a1b67cc7e73ff1d76ec0fcd409ddd3035dc394
Mubasharalig <mubasharalig325@gmail.com>
2023-09-04 16:47:30 +0500

    change achor tags to button

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -5,7 +5,7 @@ export default function Card(props) {
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-outline-primary">Check Now</a>
+                    <button style={{cursor:"pointer"}} className="btn btn-outline-primary">Check Now</button>
                 </div>
             </div>
     )
